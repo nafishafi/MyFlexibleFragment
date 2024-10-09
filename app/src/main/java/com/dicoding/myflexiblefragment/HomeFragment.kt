19653b6b22e1e6cd6d41304e7e93282787d7ea29
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2024-10-09 14:27:46 +0700

    add Category Fragment

--- app/src/main/java/com/dicoding/myflexiblefragment/HomeFragment.kt
+++ app/src/main/java/com/dicoding/myflexiblefragment/HomeFragment.kt
@@ -8,7 +8,8 @@ import android.view.ViewGroup
 import android.widget.Button
 
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), View.OnClickListener {
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
@@ -16,10 +17,19 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val btnCategory: Button = view.findViewById(R.id.btn_category)
-//        btnCategory.setOnClickListener(this)
+        val btnCategory:Button = view.findViewById(R.id.btn_category)
+        btnCategory.setOnClickListener(this)
     }
-//    override fun onClick(v: View) {
-//
-//    }
+
+    override fun onClick(v: View?) {
+        if (v?.id == R.id.btn_category) {
+            val categoryFragment = CategoryFragment()
+            val fragmentManager = parentFragmentManager
+            fragmentManager.beginTransaction().apply {
+                replace(R.id.frame_container, categoryFragment, CategoryFragment::class.java.simpleName)
+                addToBackStack(null)
+                commit()
+            }
+        }
+    }
 }
